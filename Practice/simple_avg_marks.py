2ce23d7d0fe57a03690158d4681e08b8614b785f
Rushikesh Patil <69971167+patil-rushikesh@users.noreply.github.com>
2024-01-08 12:20:02 +0530

    Create simple_avg_marks.py

---
+++ Practice/simple_avg_marks.py
@@ -0,0 +1,7 @@
+a = int(input("Enter English Marks "))
+b = int(input("Enter Hindi Marks " ))
+c = int(input("Enter Maths Marks "))
+d = int(input("Enter Science Marks "))
+e = int(input("Enter Marathi Marks "))
+f = (a+b+c+d+e)/5
+print("Average marks scored : ", f)
