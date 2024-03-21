0573f2e939f36a12285fab1d910969721224f6fa
Rushikesh Patil <prushikeh@gmail.com>
2024-03-21 14:26:06 +0530

    Added file handling code

--- Practice/filehandling.py
+++ Practice/filehandling.py
@@ -1,32 +1,32 @@
-import os
+# import os
 #Creating
-#f = open("myfile.txt", "x")
-#opening
-f = open("myfile.txt", "r")
-print(f.read())
-f.close()
-print("created File\n")
-#appending
-f = open("myfile.txt", "a")
-f.write("This is first line")
-f.close()
-print("written in a file\n")
-#opening
-f = open("myfile.txt", "r")
-print(f.read(),"\n")
-f.close()
-#overwrite
-f = open("myfile.txt", "w")
-f.write("Woops! I have deleted the content!")
-f.close()
-#opening
-f = open("myfile.txt", "r")
-print(f.read(),"\n")
-f.close()
+# f = open("xyz.txt", "x")
+# opening
+# f = open("xyz.txt", "r")
+# print(f.read())
+# f.close()
+# print("created File\n")
+# #appending
+# f = open("xyz.txt", "a")
+# f.write("This is first line")
+# f.close()
+# print("written in a file\n")
+# #opening
+# f = open("xyz.txt", "r")
+# print(f.read(),"\n")
+# f.close()
+# #overwrite
+# f = open("xyz.txt", "w")
+# f.write("Woops! I have deleted the content!")
+# f.close()
+# #opening
+# f = open("myfile.txt", "r")
+# print(f.read(),"\n")
+# f.close()
 
-#deleting
-os.remove("myfile.txt")
+# #deleting
+# os.remove("xyz.txt")
 
-f = open("myfile.txt", "r")
-print(f.read(),"\n")
-f.close()
+# f = open("xyz.txt", "r")
+# print(f.read(),"\n")
+# f.close()
